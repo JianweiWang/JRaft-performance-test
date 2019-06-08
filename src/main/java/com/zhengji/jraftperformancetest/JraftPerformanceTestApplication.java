85b92aef238455568f7a756675b8852b7828d3c7
zhengji <786594722@qq.com>
2019-06-08 16:07:55 +0800

    init project

---
+++ src/main/java/com/zhengji/jraftperformancetest/JraftPerformanceTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhengji.jraftperformancetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JraftPerformanceTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JraftPerformanceTestApplication.class, args);
+    }
+
+}
